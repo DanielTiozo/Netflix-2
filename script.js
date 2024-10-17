11d2333a91369a43ea07aa1eb91cf8f14e1ba2ba
DanielTiozo <danieltiozo@gmail.com>
2024-10-17 11:50:16 -0300

    minha versão do netflix da semana do zero

---
+++ script.js
@@ -0,0 +1,11 @@
+const avatar = document.getElementById('avatar');
+const tudu = new Audio('./audio/tudum-netflix-sound.mp3')
+const saiba = document.querySelector('.saiba');
+
+avatar.addEventListener('click', () => {
+    tudu.play();
+})
+
+saiba.addEventListener('click', () => {
+    $('#modalExemplo').modal('show')
+})
