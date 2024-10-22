f8ebf4bf4800de798bcd16675dd788a0d34a94ca
Daniel Tiozo <138174332+DanielTiozo@users.noreply.github.com>
2024-10-21 21:12:31 -0300

    projeto finalizado

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
-const avatar = document.getElementById('avatar');
-const tudu = new Audio('./audio/tudum-netflix-sound.mp3')
-const saiba = document.querySelector('.saiba');
+const botaoSom = document.querySelector(".button-som");
+const video = document.querySelector(".video");
+const botaoInfo = document.querySelector(".link-info");
+const modal = document.querySelector(".modal");
+const audio = document.querySelector(".audio");
 
-avatar.addEventListener('click', () => {
-    tudu.play();
-})
+botaoSom.addEventListener("click", ligarSom);
 
-saiba.addEventListener('click', () => {
-    $('#modal').modal('show')
-})
+function ligarSom() {
+	// if (video.muted) {
+	// 	video.muted = false;
+	// } else {
+	// 	video.muted = true;
+	// }
+	video.muted = !video.muted;
+}
+
+botaoInfo.addEventListener("click", mostrarModal);
+modal.addEventListener("click", esconderModal);
+
+function mostrarModal() {
+	modal.style.display = "block";
+}
+
+function esconderModal() {
+	modal.style.display = "none";
+}
+
+window.addEventListener("load", audioPlay);
+
+function audioPlay() {
+	audio.play();
+}
